ea1eb22ee5c6963164da36622ec3638e162bc9bd
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2021-06-19 00:35:18 +0200

    Working bookstore example

--- src/main/java/mas/Main.java
+++ src/main/java/mas/Main.java
@@ -1,8 +1,10 @@
 package mas;
 
+import client.BookStoreClient;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BookStoreClient.main(args);
     }
 }
