22c1837c49f175c67c902eab54608c7d76804980
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2021-06-22 15:47:11 +0200

    TicketService

---
+++ src/main/java/mas/service/TicketService.java
@@ -0,0 +1,25 @@
+package mas.service;
+
+import mas.entity.Ticket;
+import mas.entity.person.Client;
+import mas.entity.skatingsession.SkatingSession;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class TicketService {
+    public static BigDecimal getFinalPrice(Ticket ticket) {
+        Client client = ticket.getClient();
+        SkatingSession skatingSession = ticket.getSkatingSession();
+
+        if (client == null || client.getDiscountPercent() == null
+                || skatingSession == null || skatingSession.getTicketPrice() == null
+        ) {
+            throw new IllegalStateException();
+        }
+
+        return skatingSession.getTicketPrice()
+                .multiply(BigDecimal.valueOf(client.getDiscountPercent()))
+                .divide(BigDecimal.valueOf(100), RoundingMode.UP);
+    }
+}
